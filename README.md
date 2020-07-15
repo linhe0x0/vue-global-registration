5abd7be6bb2ff96eba56f9fba3e576f8117211bb
sqrtthree <imsqrtthree@gmail.com>
2020-07-15 15:58:36 +0800

    Make it works

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # vue-global-registration
+
 Register global Vue components with require.context.
+
+## How to use
+
+```js
+import Vue from 'vue'
+import globalRegistration from '@sqrtthree/vue-global-registration'
+
+globalRegistration(
+  require.context('./components', true, /[a-zA-Z]\w+\.(vue|js)$/)
+)(Vue)
+```
---
+++ index.js
@@ -0,0 +1,25 @@
+import _ from 'lodash'
+
+export default (context) => (Vue) => {
+  context.keys().forEach((fileName) => {
+    // Get component config
+    const componentConfig = context(fileName)
+    // Get PascalCase name of component
+    const componentName =
+      componentConfig.default.name ||
+      _.upperFirst(
+        _.camelCase(
+          // Strip the leading `./` and extension from the filename
+          fileName.replace(/^\.\/(.*)\/(.*)\.\w+$/, '$1')
+        )
+      )
+    // Register component globally.
+    Vue.component(
+      componentName,
+      // Look for the component options on `.default`, which will
+      // exist if the component was exported with `export default`,
+      // otherwise fall back to module's root.
+      componentConfig.default || componentConfig
+    )
+  })
+}
